62e10de5d198d314c9c2e2b2f0537ec934845999
meselu <meselu@meselus-Air.lan>
2023-08-16 08:53:08 -0500

    add_demo_spring_boot_application

---
+++ src/main/java/com/example/springapplication_demo_newman/SpringapplicationDemoNewmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapplication_demo_newman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapplicationDemoNewmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringapplicationDemoNewmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springapplication_demo_newman/SpringapplicationDemoNewmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapplication_demo_newman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapplicationDemoNewmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
